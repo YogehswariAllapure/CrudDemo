8aac90d9999fd79dc364237dc072fbf234ec06f4
YogehswariAllapure <yallapure@gmail.com>
2020-11-09 09:45:45 +0530

    crud operations on ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController,AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  taskList = [];
+  taskName: any;
+
+    constructor(public navCtrl: NavController,public alertCtrl: AlertController) {}
+
+    addTask() {
+        if (this.taskName.length > 0) {
+            let task = this.taskName;
+            this.taskList.push(task);
+            this.taskName = "";
+        }
+    }
+
+    async  updateTask(index) {
+      let alert = this.alertCtrl.create({
+        
+         //title: 'Update Task?',
+          message: 'Type  your new task to update.',
+          inputs: [{ name: 'editTask', placeholder: 'Task' }],
+          buttons: [{ text: 'Cancel', role: 'cancel' },
+                    { text: 'Update', handler: data => {
+                        this.taskList[index] = data.editTask; }
+                    }
+                   ]
+      });
+       (await alert).present()
+  }
+
+    deleteTask(index){
+      this.taskList.splice(index, 1);
+  }
 
 }
